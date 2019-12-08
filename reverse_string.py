87b2f86eebaf20735ce90309f711e42c97cc68c7
Sanooj Pollayil Sadananadan <sanooj.pollayilsadananadan@cognizant.com>
2019-12-08 00:08:51 +0000

    Fixed unit tets for reverse string

--- reverse_string.py
+++ reverse_string.py
@@ -12,8 +12,12 @@ def reverse_string(string_to_reverse):
         reversed_string += string_to_reverse[index]
         index -= 1
 
-    print("{}-{}".format(string_to_reverse, reversed_string))
+    # print("{}-{}".format(string_to_reverse, reversed_string))
+    return reversed_string
 
+print(reverse_string("apple"))
+print(reverse_string("hello"))
+print(reverse_string("Greetings!"))
 
 # UNITTEST
 
@@ -23,30 +27,30 @@ class TestingReverseString(unittest.TestCase):
     def test_apple_reversed_is_elppa(self):
         string_to_reverse = "Apple"
         expected = "elppA"
-        self.assertFalse(
-            expr=reverse_string(string_to_reverse),
+        self.assertTrue(
+            expr=reverse_string(string_to_reverse) == expected,
             msg="string {} is reversed".format(expected)
         )
+        # print(reverse_string(string_to_reverse) == expected)
+        # self.assertTrue(reverse_string(string_to_reverse) == expected)
 
     def test_hello_reversed_is_hello(self):
         string_to_reverse = "hello"
         expected = "olleh"
-        self.assertFalse(
-            expr=reverse_string(string_to_reverse),
+        self.assertTrue(
+            expr=reverse_string(string_to_reverse) == expected,
             msg="string {} is reversed".format(expected)
         )
 
     def test_Greetings_reversed(self):
         string_to_reverse = "Greetings!"
         expected = "!sgniteerG"
-        self.assertFalse(
-            expr=reverse_string(string_to_reverse),
+        self.assertTrue(
+            expr=reverse_string(string_to_reverse) == expected,
             msg="string {} is reversed".format(expected)   
         )
 
 
 unittest.main()
 
-reverse_string("apple")
-reverse_string("hello")
-reverse_string("Greetings!")
+
